55a07f3dc5644057df5b749b27cf3c121acc2fd4
Aaron Carry (s5063069) <s5063069@bournemouth.ac.uk>
2018-05-17 23:01:28 +0100

    gdplevel

---
+++ 2018GDP/goldberg machine/Assets/scripts/move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public bool moved;
+    public float speed;
+    Vector3 pos;
+    public float destination;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moved && gameObject.transform.position.z <= destination)
+        {
+            pos = gameObject.transform.position;
+            pos += Vector3.forward * Time.deltaTime * speed;
+            gameObject.transform.position = pos;
+        }
+        if (moved && gameObject.transform.position.z >= destination)
+        {
+            if (gameObject.transform.eulerAngles.x <= 100)
+            {
+                gameObject.transform.Rotate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+        }
+
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(moved == false)
+        {
+            moved = true;
+
+        }
+    }
+}
---
+++ 2018GDP/goldberg machine/Assets/scripts/move2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move2 : MonoBehaviour {
+    public bool moved;
+    public float speed;
+    Vector3 pos;
+    public float destination;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moved && gameObject.transform.position.y <= destination)
+        {
+            pos = gameObject.transform.position;
+            pos += Vector3.up * Time.deltaTime * speed;
+            gameObject.transform.position = pos;
+        }
+        if (moved && gameObject.transform.position.y >= destination)
+        {
+            if (gameObject.transform.eulerAngles.x <= 300)
+            {
+                gameObject.transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+        }
+
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(moved == false)
+        {
+            moved = true;
+
+        }
+    }
+}
